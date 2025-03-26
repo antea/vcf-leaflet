20f8aec1ba83083c694cf0e73f98e338280b2cd0
Riccardo Marigo <riccardo.marigo@antea.tech>
2025-03-26 11:44:25 +0100

    Closes VL-37 : Fix OverlayAddEvent and OverlayRemoveEvent.

--- src/main/java/org/vaadin/addons/componentfactory/leaflet/layer/events/OverlayRemoveEvent.java
+++ src/main/java/org/vaadin/addons/componentfactory/leaflet/layer/events/OverlayRemoveEvent.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -31,7 +31,7 @@ public class OverlayRemoveEvent extends LayersControlEvent {
   private static final long serialVersionUID = 5509563423863560531L;
 
   public OverlayRemoveEvent(LeafletMap source, boolean fromClient,
-      @EventData("event.detail.target.options.uuid") String layerId,
+      @EventData("event.detail.layer.options.uuid") String layerId,
       @EventData("event.detail.name") String name) {
     super(source, fromClient, layerId, LayerControlEventType.overlayremove, name);
   }
